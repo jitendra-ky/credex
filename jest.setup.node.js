05026f482d78b1d04b772d708e8bdb7ebb17cf36
Jitendra Kumar <jk69854@gmail.com>
2026-05-11 19:02:18 +0530

    chore: add TextEncoder polyfill for Node.js test environment

--- jest.setup.node.js
+++ jest.setup.node.js
@@ -1,5 +1,10 @@
 const dotenv = require('dotenv');
 const path = require('path');
+const { TextEncoder, TextDecoder } = require('util');
+
+// Polyfill TextEncoder/TextDecoder for pg crypto utilities
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
 
 // Load .env for tests that depend on server-side configuration.
 dotenv.config({ path: path.resolve(process.cwd(), '.env') });
