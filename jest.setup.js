31a6663d36bab9e812f9ebb0d6b7d42b0d9fdf4e
Jitendra Kumar <jk69854@gmail.com>
2026-05-11 19:27:16 +0530

    fix: correct jest.setup.js Response mock syntax

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,26 @@
 import '@testing-library/jest-dom';
+
+// Ensure Response is available for Next.js API route testing
+if (typeof globalThis.Response === 'undefined') {
+  globalThis.Response = class Response {
+    constructor(body, init) {
+      this.body = body;
+      this.init = init;
+    }
+    
+    static json(body, init) {
+      return new Response(JSON.stringify(body), {
+        ...init,
+        headers: { 'Content-Type': 'application/json', ...init?.headers },
+      });
+    }
+    
+    async json() {
+      return typeof this.body === 'string' ? JSON.parse(this.body) : this.body;
+    }
+    
+    get status() {
+      return this.init?.status || 200;
+    }
+  };
+}
